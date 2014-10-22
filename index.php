84441872e22a5a068d263cb67b4a2121d263b205
Thibaud BARDIN <thibaud.bardin@gmail.com>
2014-10-22 12:19:41 +0200

    remove slugify

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
 /**
  * @author Thibaud BARDIN (https://github.com/Irvyne).
  * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
- */ 
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
